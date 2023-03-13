3a7302cc92972b948714437619bff13f0358d111
1E10 Laptop <sample@bths.edu>
2023-03-13 11:21:48 -0400

    in

---
+++ src/Dancer.java
@@ -0,0 +1,45 @@
+public class Dancer extends Performer{
+    private int shoeSize;
+
+    public Dancer(String name, int age, Agent agent, int shoeSize){
+        super(name, age, agent);
+        this.shoeSize=shoeSize;
+    }
+
+    public void piruoette(int spins){
+        System.out.println("I am spinning " + spins + " times!");
+    }
+
+    @Override
+    public void rehearse(){
+        System.out.println("I am spinning 3 times!");
+        super.rehearse();
+    }
+
+    public void perform(boolean warmUp){
+        if(warmUp){
+            rehearse();
+        }
+        perform();
+    }
+
+    @Override
+    public void callAgent(){
+        super.callAgent();
+        System.out.println("My shoes don't fit! Please pick me up a new pair.");
+    }
+
+    public void callAgent(int correctSize){
+        callAgent();
+        System.out.println("I wear size " + correctSize + " by the way!");
+    }
+
+    public int getShoeSize(){
+        return shoeSize;
+    }
+
+    @Override
+    public String getName(){
+        return super.getName() + " is a dancer!";
+    }
+}
